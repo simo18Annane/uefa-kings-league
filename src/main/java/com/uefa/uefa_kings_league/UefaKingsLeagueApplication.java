f9200ec9cb655053437f98e646dd9aa07dd6ea25
Mohamed Annane <mohamed.annane01@gmail.com>
2025-07-28 00:38:17 +0200

    Initialisation du projet Spring Boot

---
+++ src/main/java/com/uefa/uefa_kings_league/UefaKingsLeagueApplication.java
@@ -0,0 +1,13 @@
+package com.uefa.uefa_kings_league;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UefaKingsLeagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UefaKingsLeagueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uefa/uefa_kings_league/UefaKingsLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uefa.uefa_kings_league;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UefaKingsLeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
